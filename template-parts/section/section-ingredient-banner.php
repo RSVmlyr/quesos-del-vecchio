d113eb6bfe60509a2c2fb6538d21721878491ba5
Seba <sebastian.marin@envato.com>
2025-03-31 15:22:40 -0500

    Enhance ingredient banner styles with hover effects and transitions. Update template structure to improve image positioning and link functionality.

--- template-parts/section/section-ingredient-banner.php
+++ template-parts/section/section-ingredient-banner.php
@@ -7,12 +7,13 @@ $image = get_sub_field('image');
 <link href="<?php echo get_template_directory_uri(); ?>/dist/section-ingredient-banner.css" rel="stylesheet" type="text/css" media="all">
 
 <section 
-    class="bg-beige-3 pb-14 pt-28"
+    class="bg-beige-3 pt-28"
     data-section=""
 >
     <div class="container">
-        <div
-            class="bg-orange ingredient-banner__bg grid grid-cols-[1fr,25rem,1fr] overflow-hidden rounded-[3rem] max-h-72 h-72 pt-7 px-12"
+        <a
+            href="<?php echo get_permalink( $product->ID ); ?>"
+            class="relative z-0 bg-orange ingredient-banner__bg grid grid-cols-[1fr,25rem,1fr] rounded-[3rem] max-h-72 h-72 pt-7 px-12"
             style="background-image: url(<?php echo get_template_directory_uri(); ?>/public/figures/banner-wave.svg);"
         >
             <div class="self-center">
@@ -24,17 +25,25 @@ $image = get_sub_field('image');
                     <?php echo $product->post_title; ?>
                 </h2>
             </div>
-        
-            <figure class="w-full h-auto relative">
-                <img class="w-full h-full object-contain object-bottom absolute" src="<?php echo $image["url"];?>" alt="<?php echo $image["alt"];?>" />
-            </figure>
+
+            <div class="relative w-full h-auto">
+                <figure class="absolute w-2/3 h-80 top-0 left-0 ingredient-banner__image ingredient-banner__image--left">
+                    <img class="block w-full h-full object-cover object-bottom" src="<?php echo $image["url"];?>" alt="<?php echo $image["alt"];?>" />
+                </figure>
+
+                <figure class="absolute w-2/3 h-80 top-0 right-0 ingredient-banner__image ingredient-banner__image--right">
+                    <img class="block w-full h-full object-cover object-bottom" src="<?php echo $image["url"];?>" alt="<?php echo $image["alt"];?>" />
+                </figure>
+            </div>
         
             <div class="self-center">
-                <a href="<?php echo get_permalink( $product->ID ); ?>" class="block ml-auto bg-white text-blue py-10 px-12 text-center rounded-[100%] w-full font-semibold tracking-tight max-w-80">
+                <span class="block ml-auto bg-white text-blue py-10 px-12 text-center rounded-[100%] w-full font-semibold tracking-tight max-w-80 ingredient-banner__button">
                     Ver producto
-                </a>
+                </span>
             </div>
+        </a>
 
+        <div class="bg-beige-3 relative z-10 h-14">  
         </div>
     </div>
 </section>
