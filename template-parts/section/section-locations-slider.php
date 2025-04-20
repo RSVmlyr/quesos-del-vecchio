fd048aa4aace4ac6a54429cf0c0bff303c9a5af4
Seba <sebastian.marin@envato.com>
2025-04-19 20:39:49 -0500

    Update locations slider section with new data attributes for animation effects; adjust padding for improved responsiveness and enhance layout structure for better visual appeal.

--- template-parts/section/section-locations-slider.php
+++ template-parts/section/section-locations-slider.php
@@ -10,29 +10,29 @@
 
 <section 
     data-section="LocationsSlider"
-    class="bg-beige-3 py-20 overflow-hidden"
+    class="bg-beige-3 py-8 lg:py-20 overflow-hidden"
 >
     <div class="container">
         <div class="bg-beige-2 overflow-hidden rounded-[3.5rem] px-4 py-8 lg:px-20 lg:py-20 lg:grid-cols-2 lg:grid lg:items-center">
             <div class="text-center max-w-[25rem] mb-6 lg:mb-0">
-                <p class="text-xs font-medium text-blue tracking-tight lg:text-lg lg:font-semibold">
+                <p class="text-xs font-medium text-blue tracking-tight lg:text-lg lg:font-semibold" data-animation-fade-in>
                     <?php echo $eyebrow; ?>
                 </p>
     
-                <h2 class="text-[2rem] font-medium text-blue tracking-tight leading-tight my-4 lg:text-[3.5rem]">
+                <h2 class="text-[2rem] font-medium text-blue tracking-tight leading-tight my-4 lg:text-[3.5rem]" data-animation-split-text>
                     <?php echo $title; ?>
                 </h2>
     
-                <p class="text-blue tracking-tight leading-tight max-w-64 mx-auto lg:max-w-none lg:text-2xl">
+                <p class="text-blue tracking-tight leading-tight max-w-64 mx-auto lg:max-w-none lg:text-2xl" data-animation-fade-in>
                     <?php echo $description; ?>
                 </p>
     
-                <a href="<?php echo $link['url']; ?>" class="hidden text-blue bg-orange mt-[3.25rem] rounded-[12.5rem] py-6 px-12 text-lg font-semibold tracking-tight lg:block">  
+                <a href="<?php echo $link['url']; ?>" class="hidden text-blue bg-orange mt-[3.25rem] rounded-[12.5rem] py-6 px-12 text-lg font-semibold tracking-tight lg:block" data-animation-scale>  
                     <?php echo $link['title']; ?>
                 </a>
             </div>
 
-            <div class="swiper locations-slider__swiper">
+            <div class="swiper locations-slider__swiper" data-animation-fade-in-stagger>
                 <div class="swiper-wrapper">
                     <?php foreach ($locations as $location) : 
                         setup_postdata($location);
@@ -42,7 +42,7 @@
         
                     ?>
                         <div class="swiper-slide locations-slider__slide">
-                            <a href="<?php the_permalink(); ?>" class="block bg-white rounded-2xl lg:rounded-[3rem] locations-slider__location">
+                            <a data-animation-fade-in-stagger-item href="<?php the_permalink(); ?>" class="block bg-white rounded-2xl lg:rounded-[3rem] locations-slider__location">
                                 <div class="px-3 pt-3 pb-2 lg:p-8">
                                     <h3 class="text-sm text-blue font-semibold tracking-tight leading-tight mb-2 font-figtree lg:text-[2rem] lg:font-medium lg:mb-6">
                                         <?php echo $location->post_title; ?>
